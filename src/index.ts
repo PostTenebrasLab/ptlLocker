af02d47cb2b201c0812ca956b339507094e81d01
Unknown <pliski@gmail.com>
2019-03-08 01:16:42 +0100

    [refactor] package.json
    removed webpack, karma, etc. update scripts. please delete node _modules.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { config } from 'dotenv';
-config();
+const result = config();
+
+if (result.error) {
+    throw result.error;
+}
+
+console.log(result.parsed);
+
 import { TenebrotBot } from './TenebrotBot';
 
 console.log('See this in your browser console: Typescript Webpack Starter Launched');
